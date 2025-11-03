6382e8300738d2e98a8dcf2aaf539d7948bf3636
EjoneAnalumi <ejone.analumi@student.uni-pr.edu>
2025-11-03 21:57:38 +0100

    Solution class

---
+++ src/main/java/algorithm/Solution.java
@@ -0,0 +1,11 @@
+package algorithm;
+
+public class Solution {
+    public double[] position;
+    public double fitness;
+
+    public Solution(double[] position, double fitness) {
+        this.position = position;
+        this.fitness = fitness;
+    }
+}
