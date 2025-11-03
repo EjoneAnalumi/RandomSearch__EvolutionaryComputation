62539ac20ea2bcba5b255fc3cf41395519a3d98a
EjoneAnalumi <ejone.analumi@student.uni-pr.edu>
2025-11-03 21:59:19 +0100

    Main class

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,24 @@
+package app;
+
+import problems.*;
+import algorithm.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Algorithm rs = new RandomSearch();
+
+        Problem[] problems = {
+                new Sphere(2),
+                new Sphere(5),
+                new Sphere(10)
+        };
+
+        for (Problem p : problems) {
+            int maxFes = 3000 * p.getDimensions();
+            Solution sol = rs.execute(p, maxFes, true);
+
+            System.out.println("\nBest result for " + p.getName() +
+                    " (" + p.getDimensions() + "D): " + sol.fitness);
+        }
+    }
+}
